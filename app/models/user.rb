6c8d952899bae3374837994bd364a7980fab8feb
christsooper <chalsipher@gmail.com>
2014-07-04 10:36:16 +0800

    Fix this version of modeling-users (merged new modeling-user with this version)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,9 @@ class User < ActiveRecord::Base
 
  has_secure_password
  validates :password, length: { minimum: 6 }
+ before_save { email.downcase! }
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 end
