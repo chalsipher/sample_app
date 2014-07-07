f1d387a06a476c507c251b931bde1ec0213932ff
christsooper <chalsipher@gmail.com>
2014-07-07 14:47:46 +0800

    Finish Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/home' => 'static_pages#home',    :via => 'get'
+
   match '/signup' => 'users#new',    :via => 'get'
+  match '/signin' =>'sessions#new',         :via=> 'get'
+  match '/signout' => 'sessions#destroy',     :via=> 'delete'
+
   match '/help' => 'static_pages#help',    :via => 'get'
   match '/about' => 'static_pages#about',   :via => 'get'
   match '/contact' => 'static_pages#contact', :via => 'get'
