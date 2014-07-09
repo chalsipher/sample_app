e9d7343a1216d1d7981d2a94b70888f5afffe9b3
christsooper <chalsipher@gmail.com>
2014-07-09 14:13:06 +0800

    partial chapter10 upto Showing microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   #attr_accessible :email, :name, :password, :password_confirmation,:password_digest
   attr_accessible :email, :name, :password, :password_confirmation,:admin
   #attr_accessor :password, :password_confirmation
