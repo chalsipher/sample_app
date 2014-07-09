b435a788a4ad1456ebc814bdc6de3c82de03b76d
christsooper <chalsipher@gmail.com>
2014-07-09 13:37:43 +0800

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,17 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
    def signed_in?
     !current_user.nil?
   end
@@ -28,6 +39,15 @@ module SessionsHelper
     current_user.update_attribute(:remember_token,User.digest(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+   def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
