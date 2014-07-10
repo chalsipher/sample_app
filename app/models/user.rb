77bb032edce14bc48404f10489093098820733fe
christsooper <chalsipher@gmail.com>
2014-07-10 13:26:17 +0800

    partial chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+  #has_many :microposts
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower                                 
+  has_many :followed_users, through: :relationships, source: :followed
   #attr_accessible :email, :name, :password, :password_confirmation,:password_digest
   attr_accessible :email, :name, :password, :password_confirmation,:admin
   #attr_accessor :password, :password_confirmation
@@ -31,6 +37,18 @@ class User < ActiveRecord::Base
    def feed
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
